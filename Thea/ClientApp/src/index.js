d3cb0503c230eaa53d09a94d8bf4daee6570fd1d
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2022-09-10 15:03:35 +0200

    Add strict mode

--- Thea/ClientApp/src/index.js
+++ Thea/ClientApp/src/index.js
@@ -1,19 +1,17 @@
 import 'bulma/css/bulma.min.css';
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 const root = createRoot(rootElement);
 root.render(
-  <BrowserRouter basename={baseUrl}>
+  <StrictMode>
     <App />
-  </BrowserRouter>);
+  </StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
