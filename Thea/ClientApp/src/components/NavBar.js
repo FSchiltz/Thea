c159c6c8e7bac0dea0943593ff8f06dd94b77efc
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2022-09-11 16:31:27 +0200

    Add a filter search box (#13)
    * Move settings to a component
    
    * Correct padding and add filtering
    
    * Make Sass dev dep
    
    * Fix
    
    * fix docker
    
    * remove sass
    
    * use sass instead of node-sass to help docker build

--- Thea/ClientApp/src/components/NavBar.js
+++ Thea/ClientApp/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { askNotifyPermission } from "../helpers/Notify";
 import "./NavBar.css";
+import { Settings } from "./Settings";
 
 export class NavBar extends Component {
     constructor(props) {
@@ -10,11 +11,7 @@ export class NavBar extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.onAddClick = this.onAddClick.bind(this);
-        this.handleToggle = this.handleToggle.bind(this);
-    }
-
-    handleToggle() {
-        this.setState({ collapsed: !this.state.collapsed });
+        this.onFilterChanged = this.onFilterChanged.bind(this)
     }
 
     onAddClick() {
@@ -29,58 +26,53 @@ export class NavBar extends Component {
             this.props.onNotifyChanged(e.target.checked);
     }
 
+    onFilterChanged(e) {
+        if (this.props.onFilterChanged)
+            this.props.onFilterChanged(e.target.value);
+    }
+
     render() {
         return (
             <div>
                 <nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
-                    <div className="navbar-brand">
+                    <div className="navbar-brand navbar-center">
                         <div className="navbar-item">
                             <img src="/logo.png" alt="Thea" />
                             <p className="ml-2 is-size-3">Thea</p>
                         </div>
 
-                        <div className="navbar-item ml-4">
-                            <div className="buttons">
-                                <div className="button has-background-primary" onClick={this.onAddClick}>
-                                    <span className="icon">
-                                        <svg className="feather">
-                                            <use href="/feather-sprite.svg#plus" />
+                        <div className="level is-mobile navbar-center  px-4">
+                            <div className="level-left">
+                                <div className="buttons">
+                                    <div className="button is-primary" onClick={this.onAddClick}>
+                                        <span className="icon">
+                                            <svg className="feather">
+                                                <use href="/feather-sprite.svg#plus" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="level-item is-hidden-mobile">
+                                <p className="control has-icons-left">
+                                    <input type="text" className="input is-primary filter-input" placeholder="filter" value={this.props.filter} onChange={this.onFilterChanged} />
+                                    <span className="icon is-left">
+                                        <svg className="feather" width="25" height="25">
+                                            <use href="/feather-sprite.svg#search" />
                                         </svg>
                                     </span>
-                                </div>
+                                </p>
+
+                            </div>
+
+                            <div className="level-right">
+                                <Settings notify={this.props.notify} handleChange={this.handleChange} />
                             </div>
                         </div>
                     </div>
                 </nav>
-                <div className="bottom-menu m-2">
-                    <div className="dropdown is-hoverable is-right">
-                        <div className="dropdown-trigger m-0 p-0 is-clickable" onClick={this.handleToggle}>
-                            <svg className="feather m-3" width="30" height="30">
-                                <use href="/feather-sprite.svg#settings" />
-                            </svg>
-                        </div>
-
-                        <div className="dropdown-menu">
-                            <div className="dropdown-content">
-                                <div className="dropdown-item">
-                                    <label className="checkbox m-4">
-                                        <input className="m-1" type="checkbox" checked={this.props.notify} onChange={this.handleChange} />
-                                        <span className="is-size-6 ml-2">Notify</span>
-                                    </label>
-                                </div>
-                                <div className="dropdown-item">
-                                    <a className="button is-text" href="https://github.com/FSchiltz/Thea/issues" target="_blank" rel="noreferrer">
-                                        <svg className="feather">
-                                            <use href="/feather-sprite.svg#external-link" />
-                                        </svg>
-                                        <span className="ml-2">Report bug</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         );
     }
 }
+
