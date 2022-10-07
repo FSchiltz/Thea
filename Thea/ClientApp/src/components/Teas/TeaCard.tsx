ee99fbaca9ad38d23307019cb80c7d293bd5a4cb
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2022-10-07 10:12:10 +0200

    Fix the mobile ui

--- Thea/ClientApp/src/components/Teas/TeaCard.tsx
+++ Thea/ClientApp/src/components/Teas/TeaCard.tsx
@@ -38,7 +38,7 @@ export default function TeaCard({ tea, handleEnableTea, handleDisableTea, handle
 
     return <div className={style} key={tea.id}>
         <div className='card-content px-3 py-2'>
-            <div className="level m-0">
+            <div className="level is-mobile m-0">
                 <p className='mb-1 is-size-4 ellipsis no-wrap'>{tea.name}</p>
                 {menu}
             </div>
