3140dfa0cfe5379152a53537425e83df98d6be61
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2023-05-06 12:50:16 +0200

    Background color (#46)
    * Solid color with border for the background of the card
    
    * description collapsable
    
    * Collapse at the end

--- Thea/ClientApp/src/components/Teas/TeaCard.tsx
+++ Thea/ClientApp/src/components/Teas/TeaCard.tsx
@@ -1,4 +1,5 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useState } from "react";
+
 import { formatDuration } from "../../helpers/Format";
 import Tea from "../../model/Tea";
 import TeaCardMenu from "./TeaCardMenu";
@@ -14,8 +15,6 @@ interface TeaCardProps {
 }
 
 export default function TeaCard({ tea, handleEnableTea, handleDisableTea, handleDeleteClick, handleFavoriteTea, handleClick, openEditPopup }: TeaCardProps) {
-
-
     let style = 'card card-small card-justify m-1';
 
     if (tea.isDisabled) {
@@ -37,45 +36,59 @@ export default function TeaCard({ tea, handleEnableTea, handleDisableTea, handle
     }
 
     let color: CSSProperties = {};
+    let leftColor = 'white';
+    let rightColor = 'white';
     if (tea.color) {
         // use the custom color if any
-        color.background = `linear-gradient(130deg, ${tea.color} 0%, rgba(45,253,113,0) 50%)`;
-    } else {
-        let rgb = null;
-        // else use the tea level
-        switch (tea.level) {
-            case 0:
-            default:
-                break;
-            case 1:
-                // no tea
-                rgb = '#4DD5FF';
-                break;
-            case 2:
-                rgb = '#9DFF6D';
-                break;
-            case 3:
-                rgb = '#E6C600';
-                break;
-            case 4:
-                rgb = '#E62600';
-                break;
-        }
-        if (rgb != null) {
-            color.background = `linear-gradient(130deg, ${rgb} 0%, rgba(45,253,113,0) 50%)`;
-        }
+        leftColor = tea.color;
+        color.borderBottom = `${leftColor} solid 8px`;
     }
+
+    const [expanded, setExpanded] = useState(false);
+    const toggleCardState = () => {
+        setExpanded(!expanded);
+    }
+    let collapseStyle: CSSProperties = {};
+    collapseStyle.background = 'transparent';
+
+
+    // else use the tea level
+    switch (tea.level) {
+        case 0:
+        default:
+            break;
+        case 1:
+            // no tea
+            rightColor = '#2ca02c80';
+            break;
+        case 2:
+            rightColor = '#FFEE0080';
+            break;
+        case 3:
+            rightColor = '#FF9A0080';
+            break;
+        case 4:
+            rightColor = '#FF000080';
+            break;
+    }
+
+    color.background = rightColor;
+
+    const hasDescription = !(tea.description === undefined || tea.description === null || tea.description === '');
 
     return <div className={style} key={tea.id} style={color}>
         <div className='card-content px-3 py-2'>
+
             <div className="level is-mobile m-0">
                 <p className='mb-1 is-size-4 ellipsis no-wrap'>{tea.name}</p>
+
                 {menu}
             </div>
+
             <div className='level is-mobile has-text-grey mb-2'>
                 <div className='level-left'>
                     <div className="level-item">
-                        <button className="button py-1 px-2 is-primary is-inverted" disabled={tea.isDisabled} onClick={click}>
+                        <button className="button py-1 px-2 is-primary is-inverted" style={collapseStyle} disabled={tea.isDisabled} onClick={click}>
                             <svg className="feather" width="20" height="20">
                                 <use href="/feather-sprite.svg#play" />
                             </svg>
@@ -106,10 +119,22 @@ export default function TeaCard({ tea, handleEnableTea, handleDisableTea, handle
                             <use href="/feather-sprite.svg#heart" />
                         </svg> : null}
                     </div>
+                    <div className='level-right' onClick={toggleCardState}>
+                        <div className={"button is-white collapse-header-icon" + (!expanded ? '' : '-collapse') + (hasDescription ? '' : ' is-hidden')}
+                            style={collapseStyle}>
+                            <svg className="feather" width="20" height="20">
+                                <use href="/feather-sprite.svg#chevron-up" />
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div className='content ellipsis'>{tea.description}</div>
+            <div className="collapse-content" hidden={!expanded}>
+                <div className="content ellipsis">
+                    {tea.description}
+                </div>
+            </div>
         </div>
     </div>;
 }
